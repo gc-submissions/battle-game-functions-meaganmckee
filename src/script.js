c078e31dec899ede61ef4860d2aa54a340cfe27b
Meagan <meamckee89@gmail.com>
2022-06-28 16:38:46 -0400

    mitch's solution

--- src/script.js
+++ src/script.js
@@ -5,21 +5,46 @@
 const randomDamage = () => Math.floor(Math.random() * 10 + 1); // we removed {} and return
 
 const chooseOption = (opt1, opt2) => {
-  let randNum = Math.floor(Math.random() * 2);
+  const randNum = Math.floor(Math.random() * 2);
   return randNum === 0 ? opt1 : opt2; //ternary opperator
 };
 
 const attackPlayer = function (health) {
-  return (health -= randomDamage());
+  return health - randomDamage();
 };
 
 const logHealth = (player, health) => {
-  return `${player} health: ${health}`;
+  console.log(`${player} health: ${health}`);
 };
-// console.log(logHealth("Tarzan", 20));
 
 const logDeath = (winner, loser) => {
-  return `${winner} defeated ${loser}`;
+  console.log(`${winner} defeated ${loser}`);
 };
 
-console.log(logDeath());
+const isDead = (health) => {
+  return health <= 0; //? true : false; don't need this i guess
+};
+// const isDead = (health) => health <= 0;
+
+function fight(player1, player2, player1Health, player2Health) {
+  while (true) {
+    const attacker = chooseOption(player1, player2);
+    if (attacker === player1) {
+      player2Health = attackPlayer(player2Health);
+      logHealth(player2, player2Health);
+      if (isDead(player2Health)) {
+        logDeath(player1, player2);
+        break;
+      }
+    } else {
+      player1Health = attackPlayer(player1Health);
+      logHealth(player1, player1Health);
+      if (isDead(player1Health)) {
+        logDeath(player2, player1);
+        break;
+      }
+    }
+  }
+}
+
+fight("James", "Mitch", 100, 100);
